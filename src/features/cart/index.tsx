c6ed3ede8c3f182db499267dac66cc780b618541
Pedro Lima <tsc.pedrolima@gmail.com>
2023-09-10 12:07:40 -0300

    feat: add quantity products

--- src/features/cart/index.tsx
+++ src/features/cart/index.tsx
@@ -3,20 +3,21 @@ import { RootState } from "../../redux/root-reducer";
 import { CartItemsWrapper, CartWrapper, TotalPrice } from "./style";
 
 interface ProductType {
-	id: string | number,
+	id: number,
 	name: string,
-	price: string | number,
+	price: number,
 	img: string,
+	quantity: number,
 }
 
 export const Cart = () => {
-	const { products } : {products: []} = useSelector((rootReducer: RootState) => rootReducer.cartReducer);
+	const { products } : {products: ProductType[]} = useSelector((rootReducer: RootState) => rootReducer.cartReducer);
 	console.log({products});
 	return (
 		<CartWrapper>
 			{products && products.map((product: ProductType, i: number) => (
 				<CartItemsWrapper key={i}>
-					<span>{product.name}</span> <span>R$ {product.price}</span>
+					<span>{product.quantity} {product.name}</span> <span>R$ {product.price} - ( {product.price * product.quantity} )</span>
 				</CartItemsWrapper>
 			))}
 			<TotalPrice><span>Total : </span><strong>R$ ???,??</strong></TotalPrice>
--- src/redux/cart/reducer.ts
+++ src/redux/cart/reducer.ts
@@ -1,11 +1,19 @@
-interface CartSliceType {
+interface ProductType {
+	id: number,
+	name: string,
+	price: number,
+	img: string,
+	quantity: number,
+}
+
+interface CartActionType {
 	type: "cart/addProduct",
-    payload: {
-        id: string | number,
-        name: string,
-        price: string | number,
-		img: string,
-    }
+    payload: ProductType,
+}
+
+type CartStateType = {
+	products: ProductType[],
+	totalPrice:number,
 }
 
 const initialState = {
@@ -13,12 +21,27 @@ const initialState = {
 	totalPrice: 0
 };
 
-export const cartReducer = (state = initialState, action: CartSliceType) => {
+export const cartReducer = (state: CartStateType = initialState , action: CartActionType) => {
+	// Verifica se o produto já existe
+	const productIsAlready = state.products.some((product) => product.id === action.payload.id);
+
 	switch(action.type) {
 	case "cart/addProduct":
+		// se existir aumentar a quantidade em 1
+		if (productIsAlready) {
+			return {
+				...state,
+				products: state.products.map(
+					product => product.id === action.payload.id 
+						? { ...product, quantity: product.quantity + 1} 
+						: product
+				),
+			};
+		}
+		// se não existir, apenas adiciona-lo
 		return {
-			...state, 
-			products: [...state.products, action.payload],
+			...state,
+			products: [...state.products, {...action.payload, quantity: 1 }]
 		};
 	default:
 		return state;
